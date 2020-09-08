a22dd2857ed661c418a1f03de04ee416a9d5db77
pjh0828 <495912990@qq.com>
2020-09-08 21:29:52 +0800

    p.py

---
+++ pengjianhao/p.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-  
+#====#====#====#====   
+#Author:
+#CreatDate:
+#Version: 
+#====#====#====#====
+from selenium import webdriver
+# dr=webdriver.Firefox()
+# dr.get('http://39.96.181.61/qftest-ds/')
+# e=dr.find_element_by_xpath('/html/body/form')
+# print(e.get_attribute('class'))
+# dr.quit()
+
+# dr=webdriver.Firefox()
+# dr.get('http://39.96.181.61/qftest-ds/')
+# e=dr.find_element_by_xpath('//form[1]')
+# print(e.get_attribute('id'))
+# dr.quit()
+
+# dr=webdriver.Firefox()
+# dr.get('http://39.96.181.61/qftest-ds/')
+# e=dr.find_element_by_xpath('//form[@id="loginForm"]')
+# print(e.get_attribute('id'))
+# dr.quit()
+
+# dr=webdriver.Firefox()
+# dr.get('http://39.96.181.61/qftest-ds/')
+# e=dr.find_element_by_xpath('//form[input/@name="username"]')
+# print(e.get_attribute('name'))
+# dr.quit()
+
+
+# dr=webdriver.Firefox()
+# dr.get('http://39.96.181.61/qftest-ds/')
+# e=dr.find_element_by_xpath('//form[@id="loginForm"]/input[1]')
+# print(e.get_attribute('id'))
+# dr.quit()
+
+# dr=webdriver.Firefox()
+# dr.get('http://39.96.181.61/qftest-ds/')
+# e=dr.find_element_by_xpath('//input[@name="username"]')
+# print(e.get_attribute('id'))
+# dr.quit()
+
+
+# dr=webdriver.Firefox()
+# dr.get('http://39.96.181.61/qftest-ds/')
+# e=dr.find_element_by_xpath("//input[@name='continue'][@type='submit']")
+# print(e.get_attribute('id'))
+# dr.quit()
+
+# dr=webdriver.Firefox()
+# dr.get('http://39.96.181.61/qftest-ds/')
+# e=dr.find_element_by_xpath("//*[@name='cin'][@type='submit']")
+# print(e.get_attribute('id'))
+# dr.quit()
+
+# dr=webdriver.Firefox()
+# dr.get('http://39.96.181.61/qftest-ds/')
+# e=dr.find_element_by_css_selector('class=div.c-font-normal')
+# print(e.get_attribute('id'))
+# dr.quit()
+
+
+# dr=webdriver.Firefox()
+# dr.get('http://39.96.181.61/qftest-ds/')
+# e=dr.find_elements_by_id('maker')
+# print(e.get_attribute('name'))
+# dr.quit()
+
+
+# dr=webdriver.Firefox()
+# dr.get('http://39.96.181.61/qftest-ds/')
+# e=dr.find_elements_by_name('maker')
+# print(e.get_attribute('id'))
+# dr.quit()
+
+# dr=webdriver.Firefox()
+# dr.get('http://39.96.181.61/qftest-ds/')
+# e=dr.find_elements_by_link_text('aaa')
+# print(e.get_attribute('id'))
+# dr.quit()
+
+
+# dr=webdriver.Firefox()
+# dr.get('http://39.96.181.61/qftest-ds/')
+# e=dr.find_element_by_partial_link_text('aaa')
+# print(e.get_attribute('id'))
+# dr.quit()
+
+# dr=webdriver.Firefox()
+# dr.get('http://39.96.181.61/qftest-ds/')
+# e=dr.find_elements_by_tag_name('a')
+# print(e.get_attribute('id'))
+# dr.quit()
+
+
+# dr=webdriver.Firefox()
+# dr.get('http://39.96.181.61/qftest-ds/')
+# e=dr.find_elements_by_class_name('maker')
+# print(e.get_attribute('name'))
+# dr.quit()
+
+# dr=webdriver.Firefox()
+# dr.get('http://39.96.181.61/qftest-ds/')
+# e=dr.find_elements_by_xpath("//input[@name='username']")
+# print(e.get_attribute('name'))
+# dr.quit()
+
+
+# dr=webdriver.Firefox()
+# dr.get('http://39.96.181.61/qftest-ds/')
+# e=dr.find_elements_by_css_selector()
+# print(e.get_attribute('name'))
+# dr.quit()
